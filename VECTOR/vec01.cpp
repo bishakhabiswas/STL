a135de21731289b28fc52487b18f7c995139f6b6
Bishakha Biswas <51525267+bishakhabiswas@users.noreply.github.com>
2022-02-10 13:15:23 +0600

    Add files via upload

---
+++ VECTOR/vec01.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector <int> vec;
+    vec.push_back(34) ;
+    vec.push_back(21) ;
+    vec[0]=9;
+    for(int i=0;i<vec.size();i++)
+    {
+
+
+    cout<<vec[i]<<endl;
+    }
+    cout<<vec.size()<<endl;
+    }
